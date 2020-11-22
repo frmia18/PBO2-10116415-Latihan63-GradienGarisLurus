0e5ed27396caff348804a21c16e937ad2cb04fd5
frmia18 <frmiafm@gmail.com>
2020-11-22 09:52:12 +0700

    pbo 2-10116415-latihan 62

---
+++ build/classes/pbo2/pkg10116415/latihan63/gradiengarislurus/koordinat_1.rs
@@ -0,0 +1,1 @@
+pbo2.pkg10116415.latihan63.gradiengarislurus.koordinat
---
+++ src/pbo2/pkg10116415/latihan63/gradiengarislurus/GarisLurus.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10116415.latihan63.gradiengarislurus;
+
+/**
+ *
+ * @author Server SMKS AL QUDSY
+ */
+public interface GarisLurus {
+    
+   public int hitungGradien();
+    
+}
+
